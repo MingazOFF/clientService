1faa24f25507bc28e810d4b645112d914c9d7837
Renat Mingazov <mingazofff@gmail.com>
2024-11-21 20:39:21 +0300

    empty Spring initializr

---
+++ src/test/java/ru/mingazoff/clientService/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mingazoff.clientService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
